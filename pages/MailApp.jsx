5eb588a830b415b937988912b5adf0e1763c6e77
yaelfish <winkwinksituation@gmail.com>
2019-12-25 22:41:52 +0200

    add font

---
+++ pages/MailApp.jsx
@@ -0,0 +1,11 @@
+import EmailsPage  from "../MailAPP/pages/EmailsPage.jsx";
+
+export default function MailApp(props) {
+
+    return (
+        <section>
+            <h1>HOME</h1>
+            <EmailsPage/>
+        </section>
+    )
+}
