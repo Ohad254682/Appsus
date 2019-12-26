fa2d964b3a5c2858930b079425ff2efcc3591ec0
yaelfish <winkwinksituation@gmail.com>
2019-12-26 13:37:06 +0200

    active links main nav

--- cmps/MainNav.jsx
+++ cmps/MainNav.jsx
@@ -7,10 +7,10 @@ export default class MainNav extends React.Component {
             <img src="../assets/images/logo.png" width="100"/>
             <ul>
                 <li>
-                    <NavLink to='/'>E-Mails</NavLink>
+                    <NavLink exact to='/' activeClassName="active-link">E-Mails</NavLink>
                 </li>
                 <li>
-                    <NavLink to='/notes'>Notes</NavLink>
+                    <NavLink to='/notes' activeClassName="active-link">Notes</NavLink>
                 </li>
             </ul>
         </nav>
