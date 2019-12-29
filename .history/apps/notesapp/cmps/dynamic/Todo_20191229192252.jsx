4e7f4cbfcdf29f8e220c4a5d927ed2f6799dd9c0
unknown <ohadavidar@gmail.com>
2019-12-29 19:47:37 +0200

    envelopes

---
+++ .history/apps/notesapp/cmps/dynamic/Todo_20191229192252.jsx
@@ -0,0 +1,35 @@
+import noteService from "../../services/NoteService.js";
+import TodoTask from "../TodoTask.jsx"
+export default class Todo extends React.Component {
+
+    todos = this.props.note.info.todos;
+
+    state = {
+        label: this.props.note.info.label
+    }
+
+    onChangeInput = (ev) => {
+        var value = ev.target.value;
+        var field = ev.target.name;
+        this.setState({ [field]: value });
+        noteService.editNote(this.props.note.id, value)
+            .then(this.props.onLoadNotes);
+    }
+
+    onAddTodo = () => {
+        noteService.addTodo(this.props.note.id)
+            .then(this.props.onLoadNotes);
+  }  
+
+    render() {
+        return <div>
+            <input type="text" className="textarea-note" name="label" type="text" placeholder="my to-do list" onChange={this.onChangeInput} value={this.state.label} disabled={this.props.isEditMode ? false : true}></input>
+            <ul>
+                {this.props.note.info.todos.map(todo =>
+                    <TodoTask isEditMode={this.props.isEditMode} onLoadNotes={this.props.onLoadNotes} note={this.props.note} todo={todo}></TodoTask>)}
+            </ul>
+            <button className="note-btn green" onClick={this.onAddTodo}>+</button>
+
+        </div>
+    }
+}
